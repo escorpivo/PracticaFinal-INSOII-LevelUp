6ecd2d73615d1e0efee4a8a3bbd9677e3ca50632
Marcos Fernández <mfernm42@estudiantes.unileon.es>
2025-04-16 18:23:44 +0200

    Card Component created

---
+++ levelupfront/src/Components/Game_Card.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import "./card-style.css";
+
+export const Card = ({nombre, descripcion, imagen}) => {
+  return (
+    <div className="card text-centered" style={{ width: '18rem' }}>
+        <div className='overflow'>
+          <img src={imagen} alt={nombre} className='card-img'/>
+        </div>
+        <div className='title'>
+          <h5>{nombre}</h5>
+        </div>
+        <div className='description'>
+          <p>{descripcion}</p>
+        </div>
+        
+    </div>
+  );
+};
+export default Card;
