3430212014ac40ecbfaa52d9811d25fc60bb84c1
Marcos Fernández <mfernm42@estudiantes.unileon.es>
2025-04-16 20:17:13 +0200

    Basic front created

--- levelupfront/src/Components/UI_CardsHolder.js
+++ levelupfront/src/Components/UI_CardsHolder.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import Card  from "./components/UI_Card";
+import Card  from "./UI_Card";
 
 const CardsHolder = () => {
     const [games, setGames] = useState([]);
 
     useEffect(() => {
         // Hacer una solicitud GET al backend en http://127.0.0.1:5000/games
-        axios.get("http://127.0.0.1:5000/games")
+        axios.get("http://127.0.0.1:8080/games")
             .then((response) => {
                 setGames(response.data); // Guardar los juegos en el estado
                 console.log("Datos recibidos:", response.data);
@@ -18,9 +18,9 @@ const CardsHolder = () => {
     }, []);
 
     return (
-        <div style={styles.container}>
+        <div>
             <h1>Lista de Juegos</h1>
-            <div style={styles.gamesGrid}>
+            <div>
                 {games.map((game, index) => (
 
                     <Card
