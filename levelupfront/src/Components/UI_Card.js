b66b16d04798a3646e4e97a5b7193495c576ba39
LorenaAlbillos <91281028+LorenaAlbillos@users.noreply.github.com>
2025-04-27 19:31:28 +0200

    Update: insert card rotation transition

--- levelupfront/src/Components/UI_Card.js
+++ levelupfront/src/Components/UI_Card.js
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./UI-Card-style.css";
 
-export const Card = ({nombre, descripcion, imagen}) => {
+export const Card = ({ nombre, descripcion, imagen }) => {
+  const [flipped, setFlipped] = useState(false);
+
+  const handleFlip = () => {
+    setFlipped(!flipped);
+  };
+
   return (
-    <div className="card text-centered" style={{ width: '18rem' }}>
-        <div className='overflow'>
-          <img src={imagen} alt={nombre} className='card-img'/>
-        </div>
-        <div className='title'>
+    <div className={`card ${flipped ? "flipped" : ""}`} style={{ width: '18rem' }} onClick={handleFlip}>
+      {!flipped ? (
+        <>
+          <div className='overflow'>
+            <img src={imagen} alt={nombre} className='card-img' />
+          </div>
+          <div className='title'>
+            <h5>{nombre}</h5>
+          </div>
+          <div className='description'>
+            <p>{descripcion}</p>
+          </div>
+        </>
+      ) : (
+        <div className="card-back-content">
           <h5>{nombre}</h5>
+          <p>Descripción breve del juego...</p>
+          <p>Más info aquí</p>
         </div>
-        <div className='description'>
-          <p>{descripcion}</p>
-        </div>
-        
+      )}
     </div>
   );
 };
+
 export default Card;
