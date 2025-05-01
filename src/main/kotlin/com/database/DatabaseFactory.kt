7a584ed61d64b4244fb7a88162969aa73f948b53
Samu <sgillo01@estudiantes.unileon.es>
2025-05-01 20:26:54 +0200

    Add: Añadir DatabaseFactory y tablas Exposed

---
+++ src/main/kotlin/com/database/DatabaseFactory.kt
@@ -0,0 +1,24 @@
+package com.database
+
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.transaction
+
+object DatabaseFactory {
+  fun init(url: String, user: String, pass: String) {
+    val config = HikariConfig().apply {
+      jdbcUrl = url
+      driverClassName = "org.postgresql.Driver"
+      username = user
+      password = pass
+      maximumPoolSize = 5
+    }
+    val ds = HikariDataSource(config)
+    Database.connect(ds)
+    transaction {
+      SchemaUtils.createMissingTablesAndColumns(Users, Games, Ratings, Comments)
+    }
+  }
+}
