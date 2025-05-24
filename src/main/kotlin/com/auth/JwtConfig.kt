45ca082c52f60682a1e33e0d501a3a03affee30d
escorpivo <vamigg00@estudiantes.unileon.es>
2025-05-24 21:38:34 +0200

    Fix: JwtConfig for System env variables

--- src/main/kotlin/com/auth/JwtConfig.kt
+++ src/main/kotlin/com/auth/JwtConfig.kt
@@ -9,12 +9,16 @@ import io.github.cdimascio.dotenv.dotenv
 import java.util.*
 
 object JwtConfig {
-  private val dotenv   = dotenv()
-  private val secret   = dotenv["JWT_SECRET"] ?: error("JWT_SECRET no definido")
-  private val issuer   = dotenv["JWT_ISSUER"] ?: "levelup-api"
-  private val audience = dotenv["JWT_AUDIENCE"] ?: "levelup-client"
-  private val realm    = dotenv["JWT_REALM"]    ?: "ktor sample app"
-  private val algorithm = Algorithm.HMAC256(secret)
+    //con este try catch podemos evitar errores en produccion si falta el .env
+    private val env = try { dotenv() } catch (e: Exception) { null }
+
+    //añadiendo System.getenv podemos obtener las credenciales del entorno de render
+    private val secret   = env?.get("JWT_SECRET")   ?: System.getenv("JWT_SECRET")   ?: error("JWT_SECRET no definido")
+    private val issuer   = env?.get("JWT_ISSUER")   ?: System.getenv("JWT_ISSUER")   ?: "levelup-api"
+    private val audience = env?.get("JWT_AUDIENCE") ?: System.getenv("JWT_AUDIENCE") ?: "levelup-client"
+    private val realm    = env?.get("JWT_REALM")    ?: System.getenv("JWT_REALM")    ?: "ktor sample app"
+
+    private val algorithm = Algorithm.HMAC256(secret)
 
   fun generateToken(email: String, userId: Int): String =
     JWT.create()
