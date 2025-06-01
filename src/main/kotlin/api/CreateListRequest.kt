b7d197b305919d4b0ac59c3c517dd1dac8947742
Samu <sgillo01@estudiantes.unileon.es>
2025-06-01 19:04:47 +0200

    Refactor: use GameDTO in CreateListRequest

--- src/main/kotlin/api/CreateListRequest.kt
+++ src/main/kotlin/api/CreateListRequest.kt
@@ -3,10 +3,10 @@ package api
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class GameItem(val id: Long, val name: String)
+data class GameDTO(val id: Long, val name: String)
 
 @Serializable
 data class CreateListRequest(
     val name: String,
-    val games: List<GameItem>
+    val games: List<GameDTO>
 )
